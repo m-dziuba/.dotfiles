a5b77a62d90bec1a94e7977586dd2492ce891722
Mateusz Dziuba <mateusz.dziuba97@gmail.com>
2024-01-07 14:07:44 +0100

    Correct rc.lua for awesome

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -20,10 +20,6 @@ local tyrannical = require("tyrannical")
 require("awful.hotkeys_popup.keys")
 
 
--- Load Debian menu entries
-local debian = require("debian.menu")
-local has_fdo, freedesktop = pcall(require, "freedesktop")
-
 -- {{{ Error handling
 -- Check if awesome encountered an error during startup and fell back to
 -- another config (This code will only ever execute for the fallback config)
@@ -105,7 +101,6 @@ else
     mymainmenu = awful.menu({
         items = {
                   menu_awesome,
-                  { "Debian", debian.menu.Debian_menu.Debian },
                   menu_terminal,
                   { "Browser", "firefox"},
                   { "Files", "nautilus" },
