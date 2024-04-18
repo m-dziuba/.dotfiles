29017133841c510ee06c17ef1fe93ce48409f10d
Mateusz Dziuba <mateusz.dziuba97@gmail.com>
2024-04-19 00:01:33 +0200

    Fix submodule

--- wezterm/wezterm_o/wezterm.lua
+++
@@ -1,7 +0,0 @@
-local Config = require('config')
-
-
-return Config:init()
-    :append(require("config.plugins"))
-    :append(require("config.fonts"))
-    :append(require('config.appearance')).options
